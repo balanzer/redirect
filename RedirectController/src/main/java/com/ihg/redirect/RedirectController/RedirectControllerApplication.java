1c1d387dfa5795f85841a72c9a94c8cfea670e6c
Muralitharan Varatharajan <muralitharan.varatharajan@ihg.com>
2018-01-29 08:52:24 -0500

    added sample project

---
+++ RedirectController/src/main/java/com/ihg/redirect/RedirectController/RedirectControllerApplication.java
@@ -0,0 +1,12 @@
+package com.ihg.redirect.RedirectController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectControllerApplication.class, args);
+	}
+}
